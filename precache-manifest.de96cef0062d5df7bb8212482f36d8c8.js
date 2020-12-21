43ff657e524f54e2e4ad7c997896b631f8db7bf2
Duy Tran <duyta@rever.vn>
2020-12-21 16:08:26 +0700

    deploy

---
+++ precache-manifest.de96cef0062d5df7bb8212482f36d8c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db3f590f006552bf257",
+    "url": "/ndp/css/app.aaa5a7e8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/ndp/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "717e612b410a83f682cc7b8dbeb0ab59",
+    "url": "/ndp/index.html"
+  },
+  {
+    "revision": "d8610e7df79d80c0c835",
+    "url": "/ndp/js/about.90b284f2.js"
+  },
+  {
+    "revision": "3db3f590f006552bf257",
+    "url": "/ndp/js/app.900014e2.js"
+  },
+  {
+    "revision": "aae28850e9ad8c066042",
+    "url": "/ndp/js/chunk-vendors.702076f0.js"
+  },
+  {
+    "revision": "9c69d96966ab155223ae60ef509b3231",
+    "url": "/ndp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ndp/robots.txt"
+  }
+]);
